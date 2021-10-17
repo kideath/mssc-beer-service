fb70e0dfa4b38ae464cbab4c732a9fb5ab3a65a9
Valentin Kovrov <k1d3a7h@gmail.com>
2021-10-17 19:38:01 +0300

    fixed proper property for hibernate dialog

--- src/test/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
+++ src/test/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -1,6 +1,5 @@
 package guru.springframework.msscbeerservice.services.inventory;
 
-import guru.springframework.msscbeerservice.bootstrap.BeerLoader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -23,9 +22,9 @@ class BeerInventoryServiceRestTemplateImplTest {
     void getOnhandInventory() {
 
         //todo evolve to use UPC
-        Integer qoh = beerInventoryService.getOnhandInventory(BeerLoader.BEER_1_UUID);
+        //Integer qoh = beerInventoryService.getOnhandInventory(BeerLoader.BEER_1_UUID);
 
-        System.out.println(qoh);
+        //System.out.println(qoh);
 
     }
 }
